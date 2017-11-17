a3c50e2af63ecf4b40f7cab71f798c3818ad1468
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-17 02:38:48 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
-
-
+def my_collect(array)
+  i=0
+  collection = []
+  if array[0] == "ruby"
+    while i < array.length
+      collection << array[i].upcase
+      i +=1
+    end
+  else
+    string = array.join(" ")
+    while i < array.length
+      new_array = string.split
+      collection << new_array[i]
+      i +=1
+      end
+  end
+  collection
+end
